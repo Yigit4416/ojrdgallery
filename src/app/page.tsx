45ed7c0f1ee06620f565a7f9ac14654092d2a4cd
Yigit4416 <yigit.ozdemir44@icloud.com>
2025-02-20 17:59:08 +0300

    fix small mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "../server/db";
+import { auth } from "@clerk/nextjs/server";
 
 export const dynamic = "force-dynamic"
 
 async function Images() {
+  const user = await auth();
+  const userId = user.userId!
   const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, userId), 
     orderBy: (model, { desc }) => desc(model.id)
-  })
+  });
   return (
     images.map((image) => (
       <div key={image.id} className="w-48 flex flex-col">
